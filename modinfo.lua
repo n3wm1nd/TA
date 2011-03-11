4ef91977399f9f1618d4f51872d660b0bf78aec9
newmind <git.newmind@newmind.at.eu.org>
2011-03-11 17:34:37 +0100

    version based on commit

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.03-test1',
+  version='v2.03-test-$Id$',
   mutator='Official',
   game='Total Annihilation',
   shortGame='TA',

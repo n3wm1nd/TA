75006d7306e609e32e57634b80a441107e476f88
skymyj <skymyj@hotmail.com>
2016-10-14 14:29:27 +0200

    Rr

--- units/corbhmth1.lua
+++ units/corbhmth1.lua
@@ -4,7 +4,7 @@ return {
 		activatewhenbuilt = true,
 		brakerate = 0,
 		buildangle = 8192,
-		buildcostenergy = 108260,
+		buildcostenergy = 113260,
 		buildcostmetal = 7505,
 		builder = false,
 		buildinggrounddecaldecayspeed = 30,

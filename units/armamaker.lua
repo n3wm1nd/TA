a49bf1b3b6ee3eec6fa0c7abe591e25fc9f29ce7
skymyj <skymyj@hotmail.com>
2016-11-23 09:54:20 +0100

    Update armamaker.lua

--- units/armamaker.lua
+++ units/armamaker.lua
@@ -4,7 +4,7 @@ return {
 		activatewhenbuilt = true,
 		brakerate = 0,
 		buildangle = 8192,
-		buildcostenergy = 6100,
+		buildcostenergy = 5450,
 		buildcostmetal = 105,
 		builder = false,
 		buildinggrounddecaldecayspeed = 30,
